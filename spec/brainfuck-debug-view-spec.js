6e20c74367f82ee7de01b8a9beca538275e9a697
Pablo Bangueses <pablo@bng5.net>
2016-11-15 19:12:18 -0300

    Generated package.

---
+++ spec/brainfuck-debug-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BrainfuckDebugView from '../lib/brainfuck-debug-view';
+
+describe('BrainfuckDebugView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
